d78bb1416984d8cbeafc17310a24e4c71f8122cf
ThaniOruvan25 <89347874+ThaniOruvan25@users.noreply.github.com>
2023-06-03 15:13:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,18 +4,18 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
+	API_ID = int(os.environ.get("API_ID", "7763257"))
+	API_HASH = os.environ.get("API_HASH", "52c9cbf4b4ee78eda09eb3d9ac0673d7")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6185044354:AAGZbU6dU6SGtetcqCgR-KMlzE1PbBLmYvM")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "BeatzStoreBot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001987642974"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://ThaniOruvan25:ThaniOruvan25@cluster0.cjv4s.mongodb.net/cluster0?retryWrites=true&w=majority")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001584500398")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001861476160")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
@@ -36,17 +36,18 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
 """
-	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+	HOME_TEXT2 = """
+Dear [{}](tg://user?id={})
 
-Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
-
-Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
-
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
+Welcome to @ViralBeatz Team ❤️. Thanks for Starting me.
 """
-	HOME_TEXT = """
-Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
-
-Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
+	HOME_TEXT0 = """
+<code>Adding You In Database<\code>
 """
+	HOME_TEXT1 = """
+<code>Adding to @ViralBeatz Team ❤️.<\code>
+"""
+	HOME_TEXT3 = """
+**I am a Advanced File Store Bot with some useful Tools ⚙.** 
+
+	

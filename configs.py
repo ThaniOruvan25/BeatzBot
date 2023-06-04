8ad5a834a30f12ae4e5a47526fafb35d230787ae
ThaniOruvan25 <89347874+ThaniOruvan25@users.noreply.github.com>
2023-06-04 11:55:41 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,7 +13,7 @@ class Config(object):
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://ThaniOruvan25:ThaniOruvan25@cluster0.cjv4s.mongodb.net/cluster0?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
-	TO_CHANNEL = os.environ.get("LOG_CHANNEL", "")        
+	TO_CHANNEL = os.environ.get("TO_CHANNEL", "")        
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))

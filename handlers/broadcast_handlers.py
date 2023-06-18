6849c80df8978d8d17880a20504af99c61f77890
ThaniOruvan25 <89347874+ThaniOruvan25@users.noreply.github.com>
2023-06-18 19:15:33 +0530

    Update broadcast_handlers.py

--- handlers/broadcast_handlers.py
+++ handlers/broadcast_handlers.py
@@ -88,17 +88,84 @@ async def main_broadcast_handler(m, db):
     if broadcast_ids.get(broadcast_id):
         broadcast_ids.pop(broadcast_id)
     completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
-    await asyncio.sleep(3)
+    await asyncio.sleep(1)
     await out.delete()
     if failed == 0:
         await m.reply_text(
-            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+            text=f"<b>#User #Broadcast completed in `{completed_in}`\n\n➺Total users {total_users} [/status].\n➺Total done {done},\n➺Success - {success},\n➺Failed - {failed}</b>."
             quote=True
         )
     else:
         await m.reply_document(
-            document='broadcast.txt',
-            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+            document='user-broadcast.txt',
+            caption=f"<b>#Broadcast completed in `{completed_in}`\n\n➺Total users {total_users} [/status].\n➺Total done {done},\n➺Success - {success},\n➺Failed - {failed}</b>."
             quote=True
         )
+    await asyncio.sleep(10)
     await aiofiles.os.remove('broadcast.txt')
+    await asyncio.sleep(10)
+async def bu_bd_handler(m, db):
+    all_bdusers = await db.get_all_banned_users()
+    broadcast_msg = m.reply_to_message
+    while True:
+        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+        if not broadcast_ids.get(broadcast_id):
+            break
+        fool = await m.reply_text( 
+            text=f"Broadcast Started! You will be notified with log file when all the  Banned users are notified."
+        )
+        start_time = time.time()
+        total_users = await db.total_users_count()
+        done = 0
+        failed = 0
+        success = 0
+        broadcast_ids[broadcast_id] = dict(
+            total=total_users,
+            current=done,
+            failed=failed,
+            success=success
+        )
+        async with aiofiles.open('banned-broadcast.txt', 'b') as broadcast_log_file:
+            async for user in all_users:
+                sts, msg = await send_msg(
+                    user_id=int(user['id']),
+                    message=broadcast_msg
+                )
+                if msg is not None:
+                    await broadcast_log_file.write(msg)
+                if sts == 200:
+                    success += 1
+                else:
+                    failed += 1
+                if sts == 400:
+                    await db.delete_user(user['id']) 
+                done += 1
+                if broadcast_ids.get(broadcast_id) is None:
+                    break
+                else:
+                    broadcast_ids[broadcast_id].update(
+                        dict(
+                            current=done,
+                            failed=failed,
+                            success=success
+                   )
+                )
+    if broadcast_ids.get(broadcast_id):
+        broadcast_ids.pop(broadcast_id)
+    completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
+    await asyncio.sleep(5)
+    await out.delete()
+    if failed == 0:
+        await m.reply_text(
+            text=f"<b>#Banned #Broadcast completed in `{completed_in}`\n\n➺Total users {total_users} [/status].\n➺Total done {done},\n➺Success - {success},\n➺Failed - {failed}</b>."
+            quote=True
+        )
+    else:
+        await m.reply_document(
+            document='banned-broadcast.txt',
+            caption=f"<b>#Broadcast completed in `{completed_in}`\n\n➺Total users {total_users} [/status].\n➺Total done {done},\n➺Success - {success},\n➺Failed - {failed}</b>."
+            quote=True
+        )
+    await asyncio.sleep(10)
+    await aiofiles.os.remove('banned-broadcast.txt')
+    await asyncio.sleep(10)

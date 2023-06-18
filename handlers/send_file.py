b0b56ce839c9d353fffae0e43fa3db32a84cc175
ThaniOruvan25 <89347874+ThaniOruvan25@users.noreply.github.com>
2023-06-18 13:40:51 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -3,6 +3,7 @@
 import asyncio
 from configs import Config
 from pyrogram import Client
+from datetime import datetime, timedelta
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
@@ -37,12 +38,5 @@ async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
     await asyncio.sleep(3)
-    await sent_message.delete(86400)
+    await sent_message.delete(7200)
     
-async def automatic_delete()
-    now = datetime.now()
-    target_time = now + timedelta(hours=24)
-    while now < target_time:
-        await asyncio.sleep(1)
-        now = datetime.now()
-    await sent_message.delete()

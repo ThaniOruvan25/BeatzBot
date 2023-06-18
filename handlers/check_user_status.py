ca46a12803f9b8d6d97d31a69c9e8fe78156e2c7
ThaniOruvan25 <89347874+ThaniOruvan25@users.noreply.github.com>
2023-06-18 11:38:36 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -13,16 +13,16 @@ async def handle_user_status(bot, cmd):
         await db.add_user(chat_id)
         await bot.send_message(
             Config.LOG_CHANNEL,
-            f"<b>#{cmd.from_user.first_name} #{cmd.from_user.id}\n\n➺Nᴀᴍᴇ - [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})\n➺Tɢ Iᴅ - {cmd.from_user.id}\n\n➺Tᴏᴛᴀʟ Uꜱᴇʀꜱ - <code>{db.total_users_count()}<\code><\b>"
+            f"<b>#{cmd.from_user.first_name} #{cmd.from_user.id}\n\n➺Nᴀᴍᴇ - [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})\n➺Tɢ Iᴅ - {cmd.from_user.id}\n\n➺Tᴏᴛᴀʟ Uꜱᴇʀꜱ - <code>{db.total_users_count()}<\code></b>"
         )
         
         z = await bot.send_message(
             Config.TO_CHANNEL,
-            f"<b>#{cmd.from_user.first_name} #{cmd.from_user.id} #{Config.BOT_USERNAME}\n\n➺Nᴀᴍᴇ - [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})\n➺Tɢ Iᴅ - {cmd.from_user.id}\n\n➺Tᴏᴛᴀʟ Uꜱᴇʀꜱ - <code>{db.total_users_count()}<\code><\b>"
+            f"<b>#{cmd.from_user.first_name} #{cmd.from_user.id} #{Config.BOT_USERNAME}\n\n➺Nᴀᴍᴇ - [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})\n➺Tɢ Iᴅ - {cmd.from_user.id}\n\n➺Tᴏᴛᴀʟ Uꜱᴇʀꜱ - <code>{db.total_users_count()}</code></b>"
             )
     ban_status = await db.get_ban_status(chat_id)
     if ban_status["is_banned"]:
-    await.z.edit(f"<b>#{cmd.from_user.first_name} #{cmd.from_user.id} #{Config.BOT_USERNAME}\n\n➺Nᴀᴍᴇ - [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})\n➺Tɢ Iᴅ - {cmd.from_user.id}\n\n <i>This User is Banned.<\i>➺Tᴏᴛᴀʟ Uꜱᴇʀꜱ - <code>{db.total_users_count()}<\code><\b>
+    await z.edit(f"<b>#{cmd.from_user.first_name} #{cmd.from_user.id} #{Config.BOT_USERNAME}\n\n➺Nᴀᴍᴇ - [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})\n➺Tɢ Iᴅ - {cmd.from_user.id}\n\n <i>This User is Banned.</i>➺Tᴏᴛᴀʟ Uꜱᴇʀꜱ - <code>{db.total_users_count()}</code></b>")
     ban_status = await db.get_ban_status(chat_id)
     if ban_status["is_banned"]:
         if (

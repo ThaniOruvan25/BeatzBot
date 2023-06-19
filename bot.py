35a3ee0a011c17b05732757482bccd1f0ca7186c
ThaniOruvan25 <89347874+ThaniOruvan25@users.noreply.github.com>
2023-06-19 07:46:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ from handlers.force_sub_handler import (
 from handlers.broadcast_handlers import (
     main_broadcast_handler,
     bu_bd_handler
-
+)
 from handlers.save_media import (
     save_media_in_channel,
     save_batch_media_in_channel
